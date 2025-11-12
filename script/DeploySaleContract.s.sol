a6380f58e073e7ee0d02b37d4781d3c66159f7ab
jhfnetboy <jhfnetboy@gmail.com>
2025-11-12 17:02:43 +0700

    fix: remove crate3, keep a example

--- script/DeploySaleContract.s.sol
+++ script/DeploySaleContract.s.sol
@@ -7,8 +7,8 @@ import {SaleContract} from "../src/SaleContract.sol";
 contract DeploySaleContract is Script {
     function run() external returns (SaleContract) {
         // TODO: Replace with your actual GToken address and Treasury address for deployment
-        address gTokenAddress = 0x...;
-        address treasuryAddress = 0x...;
+        address gTokenAddress = address(0); // IMPORTANT: REPLACE THIS
+        address treasuryAddress = address(0); // IMPORTANT: REPLACE THIS
         address ownerAddress = msg.sender; // Or a multisig address
 
         vm.startBroadcast();
